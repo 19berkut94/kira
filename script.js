75708e981090edb6fefee6cb43904877a434bbae
19berkut94 <70857540+19berkut94@users.noreply.github.com>
2020-09-18 19:44:53 +0300

    надеюсь ок?

--- script.js
+++ script.js
@@ -1,45 +1,4 @@
-// function name (){
-// 'use strict'
-
-    
-// document.getElementsByClassName("btn").style.backgroundColor = "white"
-//  const close = document.getElementsByClassName('close')
-//  const wind = document.getElementsByClassName('wind')
-
-
-
-// }
-
 'use strict'
-
-//THE FIRST VARIANT
-
-// const button = document.querySelector('.btn')
-// const wind = document.querySelector('.wind')
-// const close = document.querySelector('.close')
-
-// button.addEventListener('click', function() {
-//     button.classList.add('red')
-//     wind.classList.add('opened')
-
-// })
-
-// close.addEventListener('click', function(){
-//     wind.classList.remove('opened')
-// })
-//_____________2 variant________main
-/*const button = document.querySelector('.btn')
-const wind = document.querySelector('.wind')
-const close = document.querySelector('.close')
-
-button.addEventListener('click', function() {
-    wind.classList.add('appear')
-})
-
-close.addEventListener('click', function() {
-    wind.classList.remove('appear')
-})
-*/
 
 
 
@@ -47,36 +6,32 @@ const butn = document.querySelector('.btn')
 const close = document.querySelector('.close')
 const wind = document.querySelector('.wind')
 
-const modal = new function() {
 
-    this.show = function() { 
-        butn.addEventListener('click', function() {
-            wind.classList.add('appear')
-})
-    },
 
-    this.hide = function() {
-        close.addEventListener('click', function() {
-            wind.classList.remove('appear')
-            
+function Fenster(windElement) {
+
+    this.show = function(anyId) {
+        anyId.addEventListener('click', function() {
+            windElement.classList.add('appear')})
+    }
+    this.hide = function(anyId) {
+        anyId.addEventListener('click', function() {
+            windElement.classList.remove('appear')
         })
     }
-
 }
 
-modal.show();
-modal.hide();
 
-// let form = document.querySelector('form');
-// let inputName = document.getElementById('name').value;
-// let inputMail = document.getElementById('mail').value;
-// let inputPass = document.getElementById('password').value;
 
+
+const modal = new Fenster(wind) 
+modal.show(butn)
+modal.hide(close)
+ 
 butsub.onclick = function() {
 let inputName = document.getElementById('name').value;
 let inputMail = document.getElementById('mail').value;
 let inputPass = document.getElementById('password').value;
-
 
 console.log(inputName, inputMail, inputPass) 
 }
@@ -88,3 +43,75 @@ console.log(inputName, inputMail, inputPass)
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const modal = new function() {
+
+//     this.show = function() { 
+//         butn.addEventListener('click', function() {
+//             wind.classList.add('appear')
+// })
+//     },
+
+//     this.hide = function() {
+//         close.addEventListener('click', function() {
+//             wind.classList.remove('appear')
+            
+//         })
+//     }
+
+// }
+
+// modal.show();
+// modal.hide();
+
+
+
+// butsub.onclick = function() {
+// let inputName = document.getElementById('name').value;
+// let inputMail = document.getElementById('mail').value;
+// let inputPass = document.getElementById('password').value;
+
+
+// console.log(inputName, inputMail, inputPass) 
+// }
